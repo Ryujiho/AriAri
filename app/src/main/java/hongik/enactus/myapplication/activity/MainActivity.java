206b67dc28f4983fa124d437b99ea16c8465c594
jiho,ryu <goll167@gmail.com>
2021-07-26 23:59:38 +0900

    Firebase 연동 - 푸시 알람

--- app/src/main/java/hongik/enactus/myapplication/activity/MainActivity.java
+++ app/src/main/java/hongik/enactus/myapplication/activity/MainActivity.java
@@ -3,12 +3,19 @@ package hongik.enactus.myapplication.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import hongik.enactus.myapplication.R;
+import hongik.enactus.myapplication.common.Tag;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
@@ -18,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_start);
         mContext = MainActivity.this;
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(Tag.TEST, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        Log.d(Tag.TEST, "token: " + token);
+                    }
+                });
+
 
         Button btn_register_main = findViewById(R.id.btn_register_main);
         btn_register_main.setOnClickListener(new View.OnClickListener() {
